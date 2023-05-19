37ba0eb4c0502e022095b78a014468c2baed4f9a
objetive218 <davidsantiagotc@gmail.com>
2023-05-18 22:49:09 -0500

    style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
 import CountryContext from "./context/CountryContext";
 import RegionContext from "./context/RegionContext";
+import HeaderStyle from "./css/Header.module.css";
+import { BsSearch } from "react-icons/bs";
 
 const Header = () => {
   const { content, setContent } = useContext(CountryContext);
@@ -17,28 +19,36 @@ const Header = () => {
 
   return (
     <>
-      <h2>Where in the world?</h2>
-      <form>
-        <input
-          type="text"
-          placeholder="Search for a country"
-          onChange={search}
-        />
-      </form>
-      <label htmlFor="#">
-        <button>
-          <span>luna</span>
-        </button>
-      </label>
-      <label htmlFor="">
-        <select name="region" id="selregion" onChange={search1}>
-          <option value="Africa">Africa</option>
-          <option value="America">America</option>
-          <option value="Asia">Asia</option>
-          <option value="Europa">Europa</option>
-          <option value="Oceania">Oceania</option>
-        </select>
-      </label>
+      <section className={HeaderStyle.head}>
+        <h2>Where in the world?</h2>
+        <label htmlFor="#">
+          <button>
+            <span>Dark Mode</span>
+          </button>
+        </label>
+      </section>
+      <section className={HeaderStyle.bar}>
+        <form className={HeaderStyle.search}>
+          <BsSearch />
+          <input
+            type="text"
+            placeholder="Search for a country"
+            onChange={search}
+          />
+        </form>
+        <label htmlFor="" className={HeaderStyle.region}>
+          <select name="region" id="selregion" onChange={search1}>
+            <option value="" disabled selected hidden>
+              Filter by Region
+            </option>
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europa">Europa</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </label>
+      </section>
     </>
   );
 };
