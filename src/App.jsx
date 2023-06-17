04dbf4064d2c0df2c25153ae9d54f4fcd0d890eb
objetive218 <davidsantiagotc@gmail.com>
2023-06-17 11:37:52 -0500

    fetch test

--- src/App.jsx
+++ src/App.jsx
@@ -3,69 +3,64 @@ import Header from "./Header";
 import Countries from "./Countries";
 import RegionContext from "./context/RegionContext";
 import CountryContext from "./context/CountryContext";
-
 import AppStyle from "./css/App.module.css";
 
 const App = () => {
   const [paises, setPaises] = useState([]);
-  const [test, setTest] = useState([]);
+
   const { content } = useContext(CountryContext);
-  const { region, setRegion } = useContext(RegionContext);
+  const { region, test } = useContext(RegionContext);
 
-  const dates = () =>
-    fetch(`https://restcountries.com/v3.1/region/${region}`)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Something went wrong");
-      })
-      .then((res) =>
-        setPaises(
-          res.map(function (countrySel) {
-            return countrySel.name.common;
+  const dates = () => {
+    region != ""
+      ? fetch(`https://restcountries.com/v3.1/region/${region}`)
+          .then((res) => {
+            if (res.ok) {
+              return res.json();
+            }
+            throw new Error("Something went wrong");
           })
-        )
-      )
-      .catch((error) => {
-        console.log(error);
-      });
-
-  const globalDate = () =>
-    fetch(`https://restcountries.com/v3.1/all`)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Something went wrong");
-      })
-      .then((res) =>
-        setTest(
-          res.map(function (countrySel) {
-            return countrySel.name.common;
+          .then((res) =>
+            setPaises(
+              res.map(function (countrySel) {
+                return countrySel.name.common;
+              })
+            )
+          )
+          .catch((error) => {
+            console.log(error);
           })
-        )
-      )
-      .catch((error) => {
-        console.log(error);
-      });
-
-  const prueba = useRef(dates());
-
-  useEffect(
-    function () {
-      globalDate();
-    },
-    [test]
-  );
+      : fetch(`https://restcountries.com/v3.1/all`)
+          .then((res) => {
+            if (res.ok) {
+              return res.json();
+            }
+            throw new Error("Something went wrong");
+          })
+          .then((json) =>
+            setPaises(
+              json.map(function (countrySel) {
+                return countrySel.name.common;
+              })
+            )
+          )
+          .catch((error) => {
+            console.log(error);
+          });
+  };
+  useEffect(() => {
+    dates();
+  }, [region]);
 
   return (
     <>
       <Header />
       <section className={AppStyle.countries}>
         {content == null || String(content).length < 3 ? (
-          paises.map(function (element) {
-            return <Countries countrySel={element} />;
+          paises.map(function (element, index) {
+            if (index < 20) {
+              return <Countries key={index} countrySel={element} />;
+            }
           })
         ) : (
           <Countries
