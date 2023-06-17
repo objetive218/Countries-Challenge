2b53ba7ded10a348cdc1efeebd1a84624a7bcb50
objetive218 <davidsantiagotc@gmail.com>
2023-06-17 11:37:41 -0500

    globalDate

--- src/context/RegionContext.jsx
+++ src/context/RegionContext.jsx
@@ -1,10 +1,32 @@
-import { createContext, useState } from "react";
+import { createContext, useState , useEffect} from "react";
 
-const RegionContext = createContext("america");
+const RegionContext = createContext("");
 const RegionProvider = function({children}){
-  const [region, setRegion] = useState("america");
+  const [region, setRegion] = useState("");  
+  const [test, setTest] = useState([]);
+  const globalDate = () =>
+    fetch(`https://restcountries.com/v3.1/all`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Something went wrong");
+      })
+      .then((json) =>
+        setTest(
+          json.map(function (countrySel) {
+            return countrySel.name.common;
+          })
+        )
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+      useEffect(()=>{
+        globalDate()
+      },[])
 
-  return <RegionContext.Provider value={{region,setRegion}}>{children}</RegionContext.Provider>
+  return <RegionContext.Provider value={{region,setRegion, test }}>{children}</RegionContext.Provider>
 }
 export {RegionProvider}
 export default RegionContext;
