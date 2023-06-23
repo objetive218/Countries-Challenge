21bd1a6ab4529be1a99d3686cf94de2953705e9b
objetive218 <davidsantiagotc@gmail.com>
2023-06-22 23:18:30 -0500

    new state

--- src/context/CountryContext.jsx
+++ src/context/CountryContext.jsx
@@ -3,8 +3,9 @@ import { createContext, useState } from "react";
 const CountryContext = createContext(null);
 const CountryProvider = function ({ children }) {
   const [content, setContent] = useState(null);
+  const [one, setOne] = useState(null);
   return (
-    <CountryContext.Provider value={{ content, setContent }}>
+    <CountryContext.Provider value={{ content, setContent, one, setOne }}>
       {children}
     </CountryContext.Provider>
   );
