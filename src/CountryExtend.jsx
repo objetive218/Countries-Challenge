345d0f9a96a3f1259e14facb0de9b369216c5458
objetive218 <davidsantiagotc@gmail.com>
2023-06-24 22:29:04 -0500

    trying to iterate

--- src/CountryExtend.jsx
+++ src/CountryExtend.jsx
@@ -1,24 +1,53 @@
+import { useContext, useEffect, useState } from "react";
+import CountryContext from "./context/CountryContext";
+import RegionContext from "./context/RegionContext";
 
+const CountryExtend = ({ countrySel }) => {
+  const { one, setOne } = useContext(CountryContext);
+  const { global } = useContext(RegionContext);
+  const [extended, setExtended] = useState([]);
 
-export const CountryExtend = ({countrySel}) => {
+  const result = () => global.find(function (element) {
+      return element.name === countrySel;
+    });
+    const allCoun = async function () {
+      let consult = await fetch(`https://restcountries.com/v3.1/all`);
+      let result = await consult.json();
+      const busqueda = result.find(function (elemento) {
+        return elemento.name.common == countrySel;
+      });
+      setExtended(busqueda);
+    };
+
+  useEffect(() => {
+    allCoun()
+  }, [countrySel]);
   return (
     <>
-      <article className={counStyle.country} onClick={(e) => {setOne(countrySel) }}>
-        <picture className={counStyle.test}>
-
-        <img
-          className={counStyle.image}
-          src={actualizar?.flags?.svg}
-          alt="flag"
-          />
-          </picture>
-        <section className={counStyle.info}>
+      <article>
+        <picture>
+          <img src={extended?.flags?.svg} alt="flag" />
+        </picture>
+        <section>
           <h2>{countrySel}</h2>
-          <p>Population: {actualizar?.population}</p>
-          <p>Region: {actualizar?.region}</p>
-          <p>Capital: {actualizar?.capital ? actualizar?.capital : countrySel}</p>
+          <p>Population: {extended?.population}</p>
+          <p>Region: {extended?.region}</p>
+          <p>Sub Region: {extended?.subregion}</p>
+          <p>Capital: {extended?.capital ? extended?.capital : countrySel}</p>
+          <p>Top Level Domain: {extended?.tld}</p>
+          <ul>Currencies: {extended?.currencies ? Object.keys(extended?.currencies).map(element => {
+            <li>
+                {element.name}
+            </li>
+            
+          }): "error"}</ul>
+          {/*<p>Languages: {Object.keys(extended?.languages).forEach(element,index,arr => {
+            element
+          })}</p>*/}
+          <p>Border Countries: {extended?.borders}</p>
         </section>
       </article>
     </>
-  )
-}
+  );
+};
+export default CountryExtend;
