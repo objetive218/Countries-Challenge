088b4d39733a33ed42f35b025411af359bf3c9a1
objetive218 <davidsantiagotc@gmail.com>
2023-06-25 22:28:55 -0500

    countryextended 1.0

--- src/CountryExtend.jsx
+++ src/CountryExtend.jsx
@@ -7,21 +7,39 @@ const CountryExtend = ({ countrySel }) => {
   const { global } = useContext(RegionContext);
   const [extended, setExtended] = useState([]);
 
-  const result = () => global.find(function (element) {
+  const result = () =>
+    global.find(function (element) {
       return element.name === countrySel;
     });
-    const allCoun = async function () {
-      let consult = await fetch(`https://restcountries.com/v3.1/all`);
-      let result = await consult.json();
-      const busqueda = result.find(function (elemento) {
-        return elemento.name.common == countrySel;
-      });
-      setExtended(busqueda);
-    };
+  const allCoun = async function () {
+    let consult = await fetch(`https://restcountries.com/v3.1/all`);
+    let result = await consult.json();
+    const busqueda = result.find(function (elemento) {
+      return elemento.name.common == countrySel;
+    });
+    setExtended(busqueda);
+  };
 
   useEffect(() => {
-    allCoun()
+    allCoun();
   }, [countrySel]);
+
+  const current = (pro, string, bool) => {
+    const list = [];
+    if(bool){
+    for (let element in pro) {
+      const item = pro[element].name;
+      list.push(`${string} ${item} `);
+    }}else{
+      for (let element in pro) {
+        const item = pro[element];
+        list.push(`${string} ${item} `);
+    }}
+    return list;
+  };
+  const cur = current(extended?.currencies, "Current: ", true);
+  const lagu = current(extended?.languages, "Languages: ", false);
+  const border = String(extended?.borders).split(" ");
   return (
     <>
       <article>
@@ -35,16 +53,16 @@ const CountryExtend = ({ countrySel }) => {
           <p>Sub Region: {extended?.subregion}</p>
           <p>Capital: {extended?.capital ? extended?.capital : countrySel}</p>
           <p>Top Level Domain: {extended?.tld}</p>
-          <ul>Currencies: {extended?.currencies ? Object.keys(extended?.currencies).map(element => {
-            <li>
-                {element.name}
-            </li>
-            
-          }): "error"}</ul>
-          {/*<p>Languages: {Object.keys(extended?.languages).forEach(element,index,arr => {
+          <p>{cur}</p>
+          <p>
+            {lagu.map((e) => {
+              return e;
+            })}
+          </p>
+          {/*<p>Languages: {Object.keys().forEach(element,index,arr => {
             element
           })}</p>*/}
-          <p>Border Countries: {extended?.borders}</p>
+          <p>Border Countries: {border}</p>
         </section>
       </article>
     </>
