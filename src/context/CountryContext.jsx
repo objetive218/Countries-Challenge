26629931a87f104a158afbbd410e70039267edb1
objetive218 <davidsantiagotc@gmail.com>
2023-07-08 13:47:42 -0500

    border function ok

--- src/context/CountryContext.jsx
+++ src/context/CountryContext.jsx
@@ -4,8 +4,13 @@ const CountryContext = createContext(null);
 const CountryProvider = function ({ children }) {
   const [content, setContent] = useState(null);
   const [one, setOne] = useState(null);
+  const searchLog = function (e) {
+    e.preventDefault();
+    setContent(e.target.value);
+  };
+
   return (
-    <CountryContext.Provider value={{ content, setContent, one, setOne }}>
+    <CountryContext.Provider value={{ content, setContent, one, setOne, searchLog }}>
       {children}
     </CountryContext.Provider>
   );
