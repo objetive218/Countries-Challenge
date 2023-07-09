6f23f15931d73d0555d7af8dbc9a98b44cf6510e
objetive218 <davidsantiagotc@gmail.com>
2023-07-09 15:28:32 -0500

    logic v 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "./Header";
 import Countries from "./Countries";
 import RegionContext from "./context/RegionContext";
